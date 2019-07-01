7df1f83f0036e6423867bc76ac9c3254fa68c93f
Majdi Chargui <mejdichergui@gmail.com>
2019-07-01 13:02:06 +0100

    change Hello

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
 
-router.get('/services', (req, res) => {
-  console.log(req)
-  res.render('/services/services');
+router.get("/services", (req, res) => {
+  console.log(req);
+  res.render("services/services");
 });
 
 module.exports = router;
