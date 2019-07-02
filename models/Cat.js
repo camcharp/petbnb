fe18f5ea8a9d2199a26a83255303650a4576dc21
Camille Charpentier <cam.charpentier@live.fr>
2019-07-02 11:22:26 +0200

    modified cat model + css

--- models/Cat.js
+++ models/Cat.js
@@ -5,10 +5,10 @@ const catSchema = new Schema(
   {
     name: { type: String, required: true },
     age: { type: Number, required: true },
-    genre: { type: String, required: true, enum: [ 'male', 'female' ] },
+    genre: { type: String, required: true, enum: ['male', 'female'] },
     personality: {
       type: String,
-      //enum: [ 'calm', 'lazy', 'cuddly', 'loner', 'playful', 'big eater', 'grumpy', 'evil' ]
+      enum: ['calm', 'lazy', 'cuddly', 'loner', 'playful', 'big eater', 'grumpy', 'evil']
     },
     image: {type: String},
   },
@@ -23,6 +23,7 @@ const catSchema = new Schema(
 const Cat = mongoose.model('Cat', catSchema);
 module.exports = Cat;
 
-Cat.create({ name: 'Blabla', age: 2, genre: 'male', personality: 'big eater', image:'https://upload.wikimedia.org/wikipedia/commons/6/66/An_up-close_picture_of_a_curious_male_domestic_shorthair_tabby_cat.jpg' })
+/* Cat.create({ name: 'Blabla', age: 2, genre: 'female', personality: 'evil', image:'https://upload.wikimedia.org/wikipedia/commons/6/66/An_up-close_picture_of_a_curious_male_domestic_shorthair_tabby_cat.jpg' })
   .then(cat => { console.log('The cat is saved and its value is: ', cat) })
-  .catch(err => { console.log('An error happened:', err) });  
+  .catch(err => { console.log('An error happened:', err) }); 
+ */
--- models/User.js
+++ models/User.js
@@ -1,17 +1,20 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  name: {type: String, required: true},
-  lastname: {type: String, required: true},
-  email: {type: String, required: true},
-  password: {type: String, required: true},
-}, {
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at'
-  }
-});
+const userSchema = new Schema(
+	{
+		name: { type: String, required: true },
+		lastname: { type: String, required: true },
+		email: { type: String, required: true },
+		password: { type: String, required: true }
+	},
+	{
+		timestamps: {
+			createdAt: 'created_at',
+			updatedAt: 'updated_at'
+		}
+	}
+);
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
