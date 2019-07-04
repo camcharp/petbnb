6bc4841f4380ac303591713fd0b7766132f852b8
Majdi Chargui <mejdichergui@gmail.com>
2019-07-04 15:58:46 +0100

    insert cat + image

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,6 +1,6 @@
-const cloudinary = require('cloudinary');
-const cloudinaryStorage = require('multer-storage-cloudinary');
-const multer = require('multer');
+const cloudinary = require("cloudinary");
+const cloudinaryStorage = require("multer-storage-cloudinary");
+const multer = require("multer");
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
@@ -10,9 +10,9 @@ cloudinary.config({
 
 var storage = cloudinaryStorage({
   cloudinary: cloudinary,
-  folder: 'test-import', // The name of the folder in cloudinary
-  allowedFormats: ['jpg', 'png'],
-  filename: function (req, file, cb) {
+  folder: "catFile", // The name of the folder in cloudinary
+  allowedFormats: ["jpg", "png"],
+  filename: function(req, file, cb) {
     cb(null, file.originalname); // The file on cloudinary would have the same name as the original file name
   }
 });
